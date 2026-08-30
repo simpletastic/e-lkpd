f0bf0881e3fa536c9a75f4129a926f253c6855cb
simpletastic <alfanr79@gmail.com>
2026-08-31 02:07:35 +0700

    Fix bootstrap app syntax

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,6 +12,8 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->trustProxies(at: '*');
+
+        $middleware->validateCsrfTokens(except: [
             'elkpd/simpan',
         ]);
     })
